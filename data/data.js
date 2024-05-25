a19867b130c5916091794ed633815749a41b5e4a
Tieppo <tieppo.emerson.a.jr@gmail.com>
2024-05-25 18:09:28 -0300

    adicionando nivel avançado database

--- data/data.js
+++ data/data.js
@@ -1,5 +1,292 @@
 export const temas = [
     {
+// nivel basico ==================================
+        nome: "Informática Básica",
+        link: "informatica",
+        perguntas: [
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]
+    },
+    {
+        nome: "Lógica de programação",
+        link: "logicaprog",
+        perguntas: [
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]
+    },
+    {
+        nome: "Hardware",
+        link: "hardware",
+        perguntas: [
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ]
+    },
+    {
+        nome: "Office",
+        link: "office",
+        perguntas: [
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            },
+            {
+                pergunta:"",
+                responsta:"",
+                feedbackPositivo:"",
+                feedbackNegativo:"",
+                options: [
+                    "",
+                    "",
+                    "",
+                    "",
+                    "",
+                ]
+            }
+        ],
+
+
+// nivel avancado ===========================================
+
         nome: "Informática Básica",
         link: "informatica",
         perguntas: [
