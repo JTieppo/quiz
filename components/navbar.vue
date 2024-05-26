ac2f305bd03e6a8470e9a9b11e1f8427bc18a20f
Tieppo <tieppo.emerson.a.jr@gmail.com>
2024-05-25 22:55:46 -0300

    responsividade home

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="px-20 bg-teal-900 flex p-4 items-center gap-8 text-3xl">
+    <header class="px-6 xl:px-20 bg-teal-900 flex p-4 items-center gap-8 xl:text-3xl">
         <nuxt-link to="/" class="font-bold text-teal-200">Quiz</nuxt-link>
         <div class="flex gap-8 text-white">
             <nuxt-link to="/quiz/selection" class="hover:text-teal-200">Jogar</nuxt-link>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <template>
     <navbar />
-    <main class="p-10 min-h-screen scroll-smooth">
-        <section class="h-screen flex p-10">
-            <div class="mx-auto my-auto">
-                <h1 class="text-5xl mb-2">Que tal um quiz?</h1>
-                <p class="w-[500px]">Já se perguntou de onde vem tanto conhecimento? Ou mesmo como o adiquirimos? Não, não, espera... melhor que isso seria saber o quanto de conhecimento tem, agora sim ficou interessante, então que tal um quiz?</p>
+    <main class="p-6 md:p-10 min-h-screen scroll-smooth">
+        <section class="xl:h-screen flex flex-col xl:flex-row ">
+            <div class="flex flex-col mx-auto xl:my-auto">
+                <h1 class="text-3xl xl:text-5xl mb-2 mx-auto xl:mx-0">Que tal um quiz?</h1>
+                <p class="max-w-[500px] text-xs xl:text-base text-center xl:text-start">Já se perguntou de onde vem tanto conhecimento? Ou mesmo como o adiquirimos? Não, não, espera... melhor que isso seria saber o quanto de conhecimento tem, agora sim ficou interessante, então que tal um quiz?</p>
             </div>
             <nuxt-link to="/quiz/selection"
-                class="mx-auto my-auto text-white bg-teal-600 rounded-full w-96 h-96 text-8xl flex jersey transition-colors duration-300 hover:bg-teal-300 hover:text-black">
+                class="mx-auto my-10 xl:my-auto text-white bg-teal-600 rounded-full w-64 h-64 md:w-80 md:h-80 xl:w-96 xl:h-96 text-8xl flex jersey transition-colors duration-300 hover:bg-teal-300 hover:text-black">
                 <p class="my-auto mx-auto jersey">Jogar</p>
             </nuxt-link>
         </section>
-        <section class="flex flex-col px-20" id="sobre">
-            <h1 class="text-5xl mb-2">Sobre o projeto</h1>
-            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores doloremque, officiis
+        <section class="flex flex-col xl:px-20 mt-20 max-w-[300px] lg:max-w-[700px] xl:max-w-[900px] mx-auto" id="sobre">
+            <h1 class="text-3xl xl:text-5xl mx-auto xl:mx-0 mb-2">Sobre o projeto</h1>
+            <p class="text-center xl:text-start text-xs xl:text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores doloremque, officiis
                 labore
                 nisi deserunt quia veritatis dicta natus neque nemo ab eos pariatur dolores aut tempora, odit
                 eveniet
