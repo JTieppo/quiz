c0265b4394180e4c772cfa9229fc6a7f2608c15b
Tieppo <tieppo.emerson.a.jr@gmail.com>
2024-05-25 23:18:12 -0300

    1.0

--- pages/quiz/dynamic/[tema].vue
+++ pages/quiz/dynamic/[tema].vue
@@ -1,17 +1,17 @@
 <template>
     <Navbar />
-    <div class="pointer-events-none text-end px-20 mt-4">
+    <div class="pointer-events-none text-center xl:text-end px-20 mt-4">
         <p class="text-2xl">Pontuação: {{ pontuacao }}</p>
     </div>
-    <div class="flex flex-col p-20">
-        <h3 class="text-2xl w-[600px] mb-6">
+    <div class="flex flex-col p-6 sm:p-14 xl:p-20">
+        <h3 class="xl:text-2xl xl:w-[600px] mb-6">
             Então quer dizer que acredita ser um especialista em {{ tema.nome }}, vamos ver do que é capaz, boa sorte
             marujo!!
         </h3>
         <div class="flex flex-col w-full gap-4">
-            <h1 class="text-4xl">{{ tema.perguntas[i].pergunta }}</h1>
+            <h1 class="text-xl md:text-4xl">{{ tema.perguntas[i].pergunta }}</h1>
             <button :class="[
-                'rounded-lg p-2 px-4 text-2xl h-12 w-full',
+                'rounded-lg p-2 px-4 xl:text-2xl h-12 w-full',
                 verificado && opt === tema.perguntas[i].resposta ? 'bg-green-600 text-white' :
                     verificado && opt !== tema.perguntas[i].resposta ? 'bg-red-700 text-white' :
                         'bg-teal-600 hover:bg-teal-400 text-white hover:text-black'
--- pages/quiz/selection.vue
+++ pages/quiz/selection.vue
@@ -1,11 +1,17 @@
 <!-- +++++++++++++++++++++++++++++++ html ++++++++++++++++++++++++++++++++ -->
 <template>
     <navbar />
-    <div class="flex flex-col p-20">
-        <h1 class="self-center text-4xl">Hmm... então quer dizer que aceitou meu desafio?!</h1>
-        <p class="self-center">Pois então vou te dar uma "colher de chá", escolha um tema que acredita dominar e eu te farei perguntas!</p>
-        <div class="grid grid-flow-row grid-cols-4 gap-4 mt-10">
-            <nuxt-link class="bg-teal-600 hover:bg-teal-400 text-white hover:text-black rounded-lg p-2 px-4 text-2xl h-12" v-for="tema in temas" :key="tema" :to="`/quiz/dynamic/${tema.link}`" >{{tema.nome}}</nuxt-link>
+    <div class="flex flex-col p-10 xl:p-20">
+        <div class="max-w-[400px] mx-auto xl:max-w-none ">
+            <h1 class="self-center xl:text-4xl text-center xl:text-start">Hmm... então quer dizer que aceitou meu
+                desafio?!</h1>
+            <p class="self-center text-xs text-center">Pois então vou te dar uma "colher de chá", escolha
+                um tema que acredita dominar e eu te farei perguntas!</p>
+        </div>
+        <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 mt-10">
+            <nuxt-link
+                class="bg-teal-600 hover:bg-teal-400 text-white hover:text-black rounded-lg p-2 px-4 xl:text-2xl xl:h-12"
+                v-for="tema in temas" :key="tema" :to="`/quiz/dynamic/${tema.link}`">{{ tema.nome }}</nuxt-link>
         </div>
     </div>
 </template>
