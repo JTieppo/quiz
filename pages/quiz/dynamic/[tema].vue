389bb69f8cf70845c290087e488065a77c7cca76
Tieppo <tieppo.emerson.a.jr@gmail.com>
2024-05-27 14:16:20 -0300

    correção iterador questão

--- pages/quiz/dynamic/[tema].vue
+++ pages/quiz/dynamic/[tema].vue
@@ -1,6 +1,17 @@
 <template>
     <Navbar />
-    <div v-if="i <= 5">
+    <div v-if="finalizado" class="flex">
+        <div class="flex flex-col p-10 my-auto items-center mx-auto">
+            <h1 v-if="pontuacao >= 4" class="text-xl xl:text-4xl text-center">Muito bem! você me venceu marujo!</h1>
+            <h1 v-else class="text-xl xl:text-4xl text-center">Quase lá, mas boa tentativa, acredito que logo irá me
+                vencer!</h1>
+            <p class="text-xl xl:text-2xl mt-10">Sua pontuaçao: {{ pontuacao }}/5</p>
+            <nuxt-link class="flex mt-2 text-white bg-teal-500 w-40 md:w-96 rounded h-12 text-center " href="/">
+                <p class="mx-auto my-auto md:text-2xl">Voltar</p>
+            </nuxt-link>
+        </div>
+    </div>
+    <div v-else>
         <div class="pointer-events-none text-center xl:text-end px-20 mt-4">
             <p class="text-2xl">Pontuação: {{ pontuacao }}</p>
         </div>
@@ -13,7 +24,8 @@
             <div class="flex flex-col w-full gap-4">
                 <h1 class="text-xl md:text-4xl">{{ tema.perguntas[i].pergunta }}</h1>
                 <p class="text-xs lg:text-base" v-if="positivo == true">{{ tema.perguntas[i].feedbackPositivo }}</p>
-                <p class="text-xs lg:text-base" v-else-if="negativo == true">{{ tema.perguntas[i].feedbackNegativo }}</p>
+                <p class="text-xs lg:text-base" v-else-if="negativo == true">{{ tema.perguntas[i].feedbackNegativo }}
+                </p>
                 <button :class="[
                     'rounded-lg p-2 px-4 xl:text-2xl w-full',
                     verificado && opt === tema.perguntas[i].resposta ? 'bg-green-600 text-white' :
@@ -25,14 +37,6 @@
                 <button v-if="verificado" class="mt-10" @click="proxima">Próxima</button>
                 <button v-else class="mt-10 text-slate-700 opacity-50" @click="proxima" disabled>Próxima</button>
             </div>
-        </div>
-    </div>
-    <div v-else class="flex">
-        <div class="flex flex-col p-10 my-auto items-center mx-auto">
-            <h1 v-if="pontuacao >= 4" class="text-xl xl:text-4xl text-center">Muito bem! você me venceu marujo!</h1>
-        <h1 v-else class="text-xl xl:text-4xl text-center">Quase lá, mas boa tentativa, acredito que logo irá me vencer!</h1>
-        <p class="text-xl xl:text-2xl mt-10">Sua pontuaçao: {{ pontuacao }}</p>
-        <nuxt-link class="flex mt-2 text-white bg-teal-500 w-40 md:w-96 rounded h-12 text-center " href="/"><p class="mx-auto my-auto md:text-2xl">Voltar</p></nuxt-link>
         </div>
     </div>
 </template>
@@ -54,6 +58,7 @@ const valido = ref(false);
 const pontuacao = ref(0);
 const negativo = ref(false)
 const positivo = ref(false)
+const finalizado = ref(false)
 
 
 function valida(opt) {
@@ -73,10 +78,12 @@ function proxima() {
     verificado.value = false;
     positivo.value = false;
     negativo.value = false;
-    if(i<5){
-        console.log(i)
+    if (i.value < 4) {
         i.value++;
+    } else {
+        finalizado.value = true;
     }
+    console.log("i value",i.value)
 }
 
 
